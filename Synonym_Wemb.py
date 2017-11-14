7c5b762d36cbbf4e56791cc1c689bbf4eddc4609
Vikas Yadav <vikasy@email.arizona.edu>
2017-11-13 20:19:34 -0700

    Add files via upload

---
+++ Synonym_Wemb.py
@@ -0,0 +1,38 @@
+import heapq
+import math
+import ast
+import numpy as np
+
+from nltk.stem.wordnet import WordNetLemmatizer
+lmtzr = WordNetLemmatizer()
+from gensim.models import Word2Vec
+from gensim.models.keyedvectors import KeyedVectors
+
+"""
+Vocab_file=open("Vocab.txt","r")
+for line1 in Vocab_file:
+    All_words=ast.literal_eval(line1)
+
+becky_emb=open("ss_qz_04.dim50vecs.txt","r", encoding='utf-8')
+embeddings_index = {}
+# glove_emb = open('glove.6B.100d.txt','r', encoding='utf-8')
+# f = open('glove.840B.300d.txt','r', encoding='utf-8')
+
+#f = open('ss_qz_04.dim50vecs.txt')
+for line in becky_emb:
+    values = line.split()
+    word = values[0]
+    try:
+       coefs = np.asarray(values[1:], dtype='float32')
+       emb_size=coefs.shape[0]
+    except ValueError:
+       print (values[0])
+       continue
+    embeddings_index[word] = coefs
+print("Word2vc matrix len is : ",len(embeddings_index))
+print("Embedding size is: ", emb_size)
+"""
+word_vectors = KeyedVectors.load_word2vec_format('ss_qz_04.dim50vecs.txt', binary=False)
+
+word_vectors.most_similar("blood")
+
